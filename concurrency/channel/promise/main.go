8ba6e31ef81a4686d39537b273349d1cd267e1dd
Gubaydullin Nurislam <gubaydullin.nurislam@gmail.com>
2025-09-06 23:47:33 +0300

    JS promise using golang channels

---
+++ concurrency/channel/promise/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type promiseResult[T any] struct {
+	value T
+	err   error
+}
+
+type Promise[T any] struct {
+	resultChannel chan promiseResult[T]
+}
+
+func NewPromise[T any](asyncRoutine func() (T, error)) Promise[T] {
+	promise := Promise[T]{
+		resultChannel: make(chan promiseResult[T]),
+	}
+
+	go func() {
+		defer close(promise.resultChannel)
+
+		value, err := asyncRoutine()
+
+		promise.resultChannel <- promiseResult[T]{value: value, err: err}
+	}()
+
+	return promise
+}
+
+func (p *Promise[T]) Then(successCallback func(result T), errorCallback func(err error)) {
+	go func() {
+		result := <-p.resultChannel
+
+		if result.err != nil {
+			errorCallback(result.err)
+			return
+		}
+
+		successCallback(result.value)
+	}()
+}
+
+func asyncSuccessOperation() (string, error) {
+	return "ok", nil
+}
+
+func asyncFailedOperation() (string, error) {
+	return "", fmt.Errorf("Some error")
+}
+
+func main() {
+	promise := NewPromise(asyncFailedOperation)
+	promise.Then(
+		func(value string) {
+			fmt.Println(value)
+		},
+		func(err error) {
+			fmt.Printf("Got error: %s\n", err)
+		},
+	)
+
+	time.Sleep(time.Second)
+}
